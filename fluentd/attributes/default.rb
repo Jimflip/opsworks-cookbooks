f87df24351dfb2b493c2b860350113c8abce3829
James Birmingham <james@dimsum.tv>
2013-05-15 08:51:12 +0200

    removed buffer path as we are moving to memory buffer

--- fluentd/attributes/default.rb
+++ fluentd/attributes/default.rb
@@ -7,7 +7,6 @@ default['fluentd']['config']['couch']['flush_interval'] = "2s"
 
 default['fluentd']['config']['s3']['bucket'] = "rum.raw.data"
 default['fluentd']['config']['s3']['endpoint'] = "s3-eu-west-1.amazonaws.com"
-default['fluentd']['config']['s3']['buffer_path'] = "/var/log/fluent/s3"
 default['fluentd']['config']['s3']['buffer_chunk_limit'] = "256m"
 default['fluentd']['config']['s3']['flush_interval'] = "10m"
 default['fluentd']['config']['s3']['time_slice_format'] = "%Y%m%d%H%M"
